590fb27d267a1bd172f5397236d8b5599164bbe4
Samin-Sadman-Shad <saminsadmanshad@gmail.com>
2024-02-03 22:56:27 +0600

    models for card and order

--- shopAPI/MenuItemApp/models.py
+++ shopAPI/MenuItemApp/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     title = models.CharField(max_length=255, db_index=True)
+    objects = models.Manager()
 
 
 class MenuItem(models.Model):
@@ -11,3 +12,4 @@ class MenuItem(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
     featured = models.BooleanField(db_index=True)
     category = models.ForeignKey(to=Category, on_delete=models.PROTECT)
+    objects = models.Manager()
--- shopAPI/MenuItemApp/views.py
+++ shopAPI/MenuItemApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from .models import MenuItem, Category
 
 # Create your views here.
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
